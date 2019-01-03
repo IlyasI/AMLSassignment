9a0c54ed527087ff8f0497e7b702697b4f905426
ilyasi <ilyas.ibragimov@outlook.com>
2019-01-03 18:38:36 +0000

    still trying to remove large files
    Former-commit-id: aa3646393f00d13fe169db8c54d7227fb9862eb8

--- Run_CNN_Classifier.py
+++ Run_CNN_Classifier.py
@@ -2,16 +2,34 @@ from CNN_classifiers import cnn_classifier
 from ImageFiltering import run_image_filtering_from_scratch
 import sys
 
-def main():
-    #row_name on which to classify taken from first provided command line argument:
-    row_name = str(sys.argv)[1]
+def main(row_name, create_symlinks, load_trained_model, trained_model_path):
 
     cnn_classifier(
-        row_name='hair_color',
-        load_trained_model=True,
-        trained_model_path="trained_models/hair_color/Hair_color_fullmodel.h5",
-        create_symlinks=True,
+        row_name=row_name,
+        load_trained_model=load_trained_model,
+        trained_model_path=trained_model_path,
+        create_symlinks=create_symlinks,
         target_size=(128, 128),
     )
 if __name__ == "__main__":
-    main()
+    #row_name on which to classify taken from first provided command line argument:
+    row_name = str(sys.argv[1])
+    #create_symlinks taken from second command line argument provided:
+    create_symlinks = bool(sys.argv[2])
+    #load_trained_model taken from third command line argument (optional: will default to True):
+    if len(sys.argv)>3:
+        load_trained_model = bool(sys.argv[3])
+    else:
+        load_trained_model = True
+
+    if row_name == 'human':
+        main(row_name, create_symlinks, load_trained_model=load_trained_model, trained_model_path='trained_models/human/Human_fullmodel.h5')
+    elif row_name == 'eyeglasses':
+        main(row_name, create_symlinks, load_trained_model=load_trained_model, trained_model_path='trained_models/eyeglasses/Eyeglasses_fullmodel.h5')
+    elif row_name == 'hair_color':
+        main(row_name, create_symlinks, load_trained_model=load_trained_model, trained_model_path='trained_models/hair_color/Hair_color_fullmodel.h5')
+    elif row_name == 'smiling':
+        main(row_name, create_symlinks, load_trained_model=load_trained_model, trained_model_path='trained_models/smiling/Smiling_fullmodel.h5')
+    elif row_name == 'young':
+        main(row_name, create_symlinks, load_trained_model=load_trained_model, trained_model_path='trained_models/young/Young_fullmodel.h5')
+          
